0a3bb153871bf7add2ab3db5089fca258d822942
Muedgar <edgarmutangana@gmail.com>
2022-05-09 11:21:02 +0200

    9th may 2022

---
+++ plan.js
@@ -0,0 +1,18 @@
+/*finishing the portfolio project*/
+
+/*
+1.Downloading CV feature 
+    -> front end to show the user link to download the cv,
+    -> back end functionality to upload the most recent cv
+2.About me feature
+    -> finish 1000 micro projects, push all of the to github, 
+            add the statistic to this page
+    -> finish hosting 10 different web development courses materials on udemy,
+            document proof, add the statistic to this page
+    -> finish saling 500 projects to 500 different customers, 
+            document proof, add the statistic to this page
+    -> finish getting 500 different customer reviews, 
+            document proof, add the statistic to this page
+    -> save all documented proof to my back end system.
+    
+*/
